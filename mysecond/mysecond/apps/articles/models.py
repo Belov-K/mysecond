1d18f196395fb9134d50c5d2b7dbc29021d83857
kiril <kaz1nak3@mail.ru>
2023-06-18 20:01:06 +0300

    feat(article): добавил статьи и комментарии

--- mysecond/mysecond/apps/articles/models.py
+++ mysecond/mysecond/apps/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    article_title=models.CharField('Название статьи', max_lenght=200)
+    article_text=models.TextField('Текст статьи')
+    pub_date=models.DateTimeField('дата публикации')
+
+class Comment(models.Model):
+    article=models.ForeignKey(Article, on_delete=models.CASCADE)
+    authon_name=models.CharField('имя пользователя', max_lenght=50)
+    comment_text=models.CharField('Текст комментария', max_lenght=200)
